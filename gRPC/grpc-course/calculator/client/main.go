56e2ab41abf013aaa586f24281772f5f727293fc
QuanDN22 <dinhngocquan112378@gmail.com>
2024-04-15 10:23:16 +0700

    use grpc calculator two number

---
+++ gRPC/grpc-course/calculator/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "grpc-go-course/calculator/proto"
+)
+
+var addr string = "localhost:50051"
+
+func main() {
+	//create a connection
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewCalculatorServiceClient(conn)
+	doSum(c)
+}
---
+++ gRPC/grpc-course/calculator/client/sum.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	pb "grpc-go-course/calculator/proto"
+	"log"
+)
+
+func doSum(c pb.CalculatorServiceClient) {
+	log.Println("doSum was invoked") 
+	res, err := c.Sum(context.Background(), &pb.SumRequest{
+		FirstNumber: 1,
+		SecondNumber: 2,
+	}) 
+
+	if err != nil {
+		log.Fatalf("Could not sum: %v\n", err)
+	}
+
+	log.Printf("Sum : %v\n", res.Result)
+}
